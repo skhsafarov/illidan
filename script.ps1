29b1efc8ef93a6909b8f26b432602be44b678093
Sardor <skhsafarov@edu.hse.ru>
2023-04-28 10:06:28 +0300

    saved master branch

--- script.ps1
+++ script.ps1
@@ -1,18 +1,17 @@
-﻿git checkout master
+﻿git checkout main
 git add *
 git commit -m "saved master branch"
 git push
 
 
+# удалить в папке ".published/illidan/" все файлы и папки, кроме папки ".git"
+Remove-Item -Path .published/illidan/* -Exclude .git -Recurse -Force
 
-#move bin/Release/net6.0/illidan/.git bin/Release/net6.0
-#remove-item -path bin/Release/net6.0/illidan/* -recurse
-#move bin/Release/net6.0/.git bin/Release/net6.0/illidan
-#remove-item -path bin/Release/net6.0/Published/* -recurse
-#dotnet publish -c Release -o bin/Release/net6.0/Published
-#cd bin/Release/net6.0/illidan
-#move ../Published/wwwroot/*
-#git checkout gh-pages
-#git add *
-#git commit -m "published gh-pages branch"
-#git push
+dotnet publish -c Release -o .published/illidan
+
+# cd .published/illidan
+# git checkout gh-pages
+# git add *
+# git commit -m "published gh-pages branch"
+# git push
+# cd ../../
