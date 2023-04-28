b2cc6dbf5eb8119d2acc7f723935dc29641007cc
Sardor <skhsafarov@edu.hse.ru>
2023-04-28 10:36:46 +0300

    saved master branch

--- script.ps1
+++ script.ps1
@@ -6,18 +6,28 @@ git push
 
 # удалить всё кроме папки с названием ".git"
 Remove-Item -Path .published/illidan/* -Recurse -Exclude .git
+dotnet publish illidan_Client/illidan_Client.csproj -c Release -o illidan_Client/bin/Release/net6.0/Published
+move illidan_Client/bin/Release/net6.0/Published/wwwroot/* .published/illidan
+cd .published/illidan
+# удалить файл "manifest.json"
+Remove-Item -Path manifest.json
+# переименовать файл "release.json" в "manifest.json"
+Rename-Item -Path release.json -NewName manifest.json
+# удалить файл "index.html"
+Remove-Item -Path index.html
+# создать копию файла "404.html" с названием "index.html"
+Copy-Item -Path "404.html" -Destination "index.html"
+git checkout gh-pages
+git add *
+git commit -m "published gh-pages branch"
+git push
 
-
+cd ../../
 
 
 #move bin/Release/net6.0/illidan/.git bin/Release/net6.0
 #remove-item -path bin/Release/net6.0/illidan/* -recurse
 #move bin/Release/net6.0/.git bin/Release/net6.0/illidan
 #remove-item -path bin/Release/net6.0/Published/* -recurse
-#dotnet publish -c Release -o bin/Release/net6.0/Published
 #cd bin/Release/net6.0/illidan
 #move ../Published/wwwroot/*
-#git checkout gh-pages
-#git add *
-#git commit -m "published gh-pages branch"
-#git push
