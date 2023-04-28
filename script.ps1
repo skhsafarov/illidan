a17c4d070e65d400cfb2f138e1dc44daa7b255b4
Sardor Safarov <skhsafarov@edu.hse.ru>
2023-04-28 11:29:10 +0300

    saved main branch

--- script.ps1
+++ script.ps1
@@ -1,6 +1,6 @@
 ﻿git checkout main
 git add *
-git commit -m "saved master branch"
+git commit -m "saved main branch"
 git push
 
 
@@ -10,17 +10,17 @@ dotnet publish illidan_Client/illidan_Client.csproj -c Release -o illidan_Client
 move illidan_Client/bin/Release/net6.0/Published/wwwroot/* .published/illidan
 cd .published/illidan
 # удалить файл "manifest.json"
-# Remove-Item -Path manifest.json
-# # переименовать файл "release.json" в "manifest.json"
-# Rename-Item -Path release.json -NewName manifest.json
-# # удалить файл "index.html"
-# Remove-Item -Path index.html
-# # создать копию файла "404.html" с названием "index.html"
-# Copy-Item -Path "404.html" -Destination "index.html"
-# git checkout gh-pages
-# git add *
-# git commit -m "published gh-pages branch"
-# git push
+Remove-Item -Path manifest.json
+# переименовать файл "release.json" в "manifest.json"
+Rename-Item -Path release.json -NewName manifest.json
+# удалить файл "index.html"
+Remove-Item -Path index.html
+# создать копию файла "404.html" с названием "index.html"
+Copy-Item -Path "404.html" -Destination "index.html"
+git checkout gh-pages
+git add *
+git commit -m "published gh-pages branch"
+git push
 
 
 
